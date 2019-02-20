e10887faaafee01c15412a76c8d592bdcc630933
Jacob Cook <jc2017@ic.ac.uk>
2019-02-20 10:41:40 +0000

    Adding some folder structure

---
+++ DethStats/basic.jl
@@ -0,0 +1,13 @@
+#!/usr/bin/env julia
+# basic.jl
+# A script to read in the parameters and paths and then use to determine
+# entropy produced along the path and action of forward and reverse paths
+#
+# Author: Jacob Cook
+# Date: February 2019
+
+function main()
+    return(nothing)
+end
+
+@time main
