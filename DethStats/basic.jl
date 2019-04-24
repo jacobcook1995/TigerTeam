ddd6a914de297829cc49f318c55836979fe13af2
Jacob Cook <jc2017@ic.ac.uk>
2019-04-24 11:54:10 +0100

    wrote basic function, now going for lunch

--- DethStats/basic.jl
+++ DethStats/basic.jl
@@ -6,6 +6,29 @@
 # Author: Jacob Cook
 # Date: February 2019
 
+function pearson(xvec::Array{Float64,1},yvec::Array{Float64,1})
+    # check two lists are the same length
+    if length(xvec) != length(yvec)
+        println("Error: Cannot find correlation of data sets that don't match.")
+        error()
+    end
+    # need to include a step to remove NaNs from consideration
+    # Now calculate Pearson correlation coefficient
+    xbarT = sum(xvec)/length(xvec)
+    ybarT = sum(yvec)/length(yvec)
+    a = 0
+    b = 0
+    c = 0
+    for i = 1:length(xvec)
+        a += (xvec[i] - xbarT)*(yvec[i] - ybarT)
+        b += (xvec[i] - xbarT)^2
+        c += (yvec[i] - ybarT)^2
+    end
+    r = a/sqrt(b*c)
+    P = 0
+    return(r,P)
+end
+
 function main()
     # Check there is a file of productions to be read
     infile = "../Input/soildata.csv"
@@ -60,8 +83,12 @@ function main()
     OrgC = parse.(Float64,soildata[2:end,9]) # percentage carbon Organic matter
     OrgNratio = parse.(Float64,soildata[2:end,10])
     CNratio = parse.(Float64,soildata[2:end,11])
-    # Data is now appropriately processed
-    
+    # should find data ranges for grasslands/woodlands/grassslands-brigant
+    Bgrassrange = 1:49 # all grasslands
+    woodrange = 50:94 # coal spoil (all) woodlands
+    grassrange = 1:40 # coal spoil grasslands
+    # Data is now appropriately processed need to think what stats I want to do
+    println(pearson(alt,alt))
     return(nothing)
 end
 
